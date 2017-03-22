911b26e0e40b2441ad6f98594e995e5361a4fe04
Sean Vaskov <skvaskov@umich.edu>
2017-03-22 10:34:39 -0400

    update

--- simulateresultscurve.m
+++ simulateresultscurve.m
@@ -1,6 +1,7 @@
 clear
 %simulate results for random inputs
 datastruct = load('reprocessedmarchlinedup.mat') ;
+load('results_3_21_17.mat')
 tarray=[17,25];
 carray=['r','b','k','c','m'];
 
@@ -12,11 +13,12 @@ carray=['r','b','k','c','m'];
 %p=[2.759,1.1098,1.585,3.000,.0093,12.1213,15.1396];
 
 %solved for on trials 17,10,14,15,20,22 only caring about states 4,5 in cost(first order opt e-7)
-p=[2.756,.5,2,2.9,1.32e-5,50,780];
+%p=[2.756,.5,2,2.9,1.32e-5,50,780];
+p=results(4,1:7);
 
 
-start=25;
-fin=50;
+start=50;
+fin=100;
 
 figure
 for i=1:length(tarray)
